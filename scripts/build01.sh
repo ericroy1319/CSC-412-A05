26cbc7fffd70bf66b2dd46e0b3326025efa8a055
ericroy1319 <ericroy1319@uri.edu>
2022-11-14 13:16:30 -0500

    Version01 complete including scripts

--- scripts/build01.sh
+++ scripts/build01.sh
@@ -2,7 +2,10 @@
 # compile code from root folder 
 g++ -std=c++17 'programs/version1/main.cpp' -o prog
 # exectute the program from the root folder 
-./prog 2 "/Users/ericroy/Documents/uri/Fall_2022/CSC-412/Data-Sets/Large-Data-Sets/DataSet-1--2-procs" "programs/version1"
+# $1 is the number of children 
+# $2 is the path to the data folder 
+# $3 is the path to the scrap folder 
+./prog $1 $2 $3
 
 # Reomve the executable from the root folder 
 rm prog
--- scripts/script01.sh
+++ scripts/script01.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
-#temp=$(cut -d' ' -f1 $FILE)
+
+# Save the root folder path 
+pwd=$(pwd)
+
+# create variable to store child count 
 c_num=0
-# search the directory for all files of type .txt 
+
+# search the directory for all files of type .txt
+#  navigate to directory 
 cd $1;
+# loop through directory and seach each file 
 for FILE in *; do 
-    temp=$(cut -b 1 $FILE)
+    # take the first byte from each file, will contain child process number 
+    temp=$(cut -d' ' -f1 $FILE)
+    # if the current process number is larger then child count, set c_cum to larger number 
     if [[ $temp -gt $c_num ]]; then 
         c_num=$temp;
     fi
 done
 
-echo "$c_num"
+# Navigate back to root folder 
+cd "$pwd"
+c_num=$((c_num+1))
+# call build 
+# c_num is the number of children 
+# $1 is the path to the data folder 
+# $2 is the path to the scrap folder 
+./scripts/build01.sh $c_num $1 $2
