109837eddb41bfb100f58b1776b700b9917b56f5
ericroy1319 <ericroy1319@uri.edu>
2022-11-14 10:28:43 -0500

    scripts work

--- scripts/build01.sh
+++ scripts/build01.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-cd 'programs/version1/'
-g++ -std=c++17 main.cpp -o prog
-./prog
+# compile code from root folder 
+g++ -std=c++17 'programs/version1/main.cpp' -o prog
+# exectute the program from the root folder 
+./prog 5 "/Users/ericroy/Documents/uri/Fall_2022/CSC-412/CSC-412-A05/Data-Sets/Data-Set-1" "programs/version1"
+
+# Reomve the executable from the root folder 
 rm prog
+# Remove the scrap directory from the programs/version1 directory
+# rmdir 'programs/version1/scrap'
