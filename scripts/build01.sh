99484a12bc553685e62632f1b71a73fe56e3e1b2
ericroy1319 <ericroy1319@uri.edu>
2022-11-14 11:45:02 -0500

    tested version 1 with large-dataset-1-2

--- scripts/build01.sh
+++ scripts/build01.sh
@@ -2,9 +2,7 @@
 # compile code from root folder 
 g++ -std=c++17 'programs/version1/main.cpp' -o prog
 # exectute the program from the root folder 
-./prog 4 "/Users/ericroy/Documents/uri/Fall_2022/CSC-412/Data-Sets/Data-Set-1" "programs/version1"
+./prog 2 "/Users/ericroy/Documents/uri/Fall_2022/CSC-412/Data-Sets/Large-Data-Sets/DataSet-1--2-procs" "programs/version1"
 
 # Reomve the executable from the root folder 
 rm prog
-# Remove the scrap directory from the programs/version1 directory
-# rmdir 'programs/version1/scrap'
